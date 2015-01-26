15d941fa88542065a3d0d0bd244cbdce773f49d5
fquezadagh <felipe_quezada@outlook.com>
2015-01-25 19:28:41 -0500

    Final (I think)

--- cachematrix.R
+++ cachematrix.R
@@ -2,15 +2,38 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## This function get the given matrix (mat) and returns the inverse
+##iniciar() set the values of the matrix
+##obtener() get the values of the matrix
 makeCacheMatrix <- function(x = matrix()) {
   ##My file
+  invmat<-NULL
 
+  iniciar<-function(y)
+  {
+    x<<-y
+    invmat<<-NULL
+  }
+  
+  obtener<-function() x
+  iniciarmat<-function(solve) invmat<<- solve
+  obtenermat<-function() invmat
+  list(iniciar=iniciar,obtener=obtener,iniciarmat=iniciarmat, obtenermat=obtenermat)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x=matrix(), ...) {
         ## Return a matrix that is the inverse of 'x'
+  mat<-x$obtenermat()
+  if(!is.null(mat))
+  {
+    message("getting inverse")
+    return(mat)
+  }
+  matrix<-x$get()
+  mat<-solve(matrix,...)
+  x$iniciarmat(mat)
+  mat
 }
