0d9bb88ac64c20ae3a6eae0089a36de3d8efb229
fquezadagh <felipe_quezada@outlook.com>
2015-01-25 19:51:01 -0500

    Final with comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,9 +3,10 @@
 
 ## Write a short comment describing this function
 ## This function get the given matrix (mat) and returns the inverse
-##iniciar() set the values of the matrix
-##iniciarmat() set the values of the inverse matrix
-##obtener() get the values of the matrix
+##iniciar() set the objects on the parent environment
+##obtener() gets the matrix to be inversed
+##iniciarmat() solves the input matrix and inverses it, returning the inversed matrix
+##obtenermat() get the inversed matrix
 makeCacheMatrix <- function(x = matrix()) {
   ##My file
   invmat<-NULL
@@ -14,7 +15,6 @@ makeCacheMatrix <- function(x = matrix()) {
     x<<-y
     invmat<<-NULL
   }
-  
   obtener<-function() x
   iniciarmat<-function(solve) invmat<<- solve
   obtenermat<-function() invmat
@@ -23,13 +23,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+##This function searchs the current environment and the parent environment
+##for the "invmat" value, if it finds the value returns it and if the value is null
+##this function calculate the inverse of the given matrix and stores it in the parent
+##environment (cached)
 cacheSolve <- function(x=matrix(), ...) {
         ## Return a matrix that is the inverse of 'x'
   invmat<-x$obtenermat()
   if(!is.null(invmat))
   {
-    message("getting inverse")
+    message("getting inverse") ##It may takes a while if the matrix is big enough
     return(invmat)
   }
   matrix<-x$obtener()
